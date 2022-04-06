6c8a933f485f84ddc4c41fd5c7f894087421abb8
adw62 <alexw1243@gmail.com>
2022-04-06 17:17:58 +0100

    Remove extra line in single rep header

--- TIES_MD/eng_scripts/namd_single_rep/sub.sh
+++ TIES_MD/eng_scripts/namd_single_rep/sub.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 {header}
 
-#--nodes and nodes_per_namd can be scaled up for large simulations
-nodes_per_namd={nodes_per_namd}
-cpus_per_namd= 128 \* $nodes_per_namd #128 cpus per node on ARCHER2
-
-#change this line to point to your project
-ties_dir={root}
-cd $ties_dir/replica-confs
-
 for stage in {{0..2}}; do
 win_id=0
 for lambda in {lambs}; do
