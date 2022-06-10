1a83c82b6b5f1f853732e315a37074e219a5b824
A <alexw1243@gmail.com>
2022-06-10 09:50:59 +0100

    add openmmtools licence

--- TIES_MD/openmmtools/alchemy.py
+++ TIES_MD/openmmtools/alchemy.py
@@ -1,6 +1,29 @@
+#!/usr/bin/python
+
+__copyright__ = """MIT License
+
+Copyright (c) 2015-2019 Chodera lab // Memorial Sloan Kettering Cancer Center
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE."""
+
 import copy
 import itertools
-import logging
 
 import openmmtools
 from openmmtools.constants import ONE_4PI_EPS0
@@ -10,9 +33,6 @@ try:
     from openmm import unit
 except ImportError:  # OpenMM < 7.6
     from simtk import openmm, unit
-
-
-logger = logging.getLogger(__name__)
 
 class AlchemicalStateError(states.GlobalParameterError):
     """Error raised by an AlchemicalState."""
